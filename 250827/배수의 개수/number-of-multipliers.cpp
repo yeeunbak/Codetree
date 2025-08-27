4e8d58c1c5bd4482f7d6ae5935738414adc950aa
yeeunbak <67646339+yeeunbak@users.noreply.github.com>
2025-08-27 16:45:28 +0900

    Title: 배수의 개수, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

--- 250827/배수의 개수/number-of-multipliers.cpp
+++ 250827/배수의 개수/number-of-multipliers.cpp
@@ -4,21 +4,21 @@ using namespace std;
 int main() {
     // Please write your code here.
     int A[10];
-    for(int i=0;i<10;i++)
-    {
-        cin >> A[i];
-    }
 
     int cnt_3=0;
     int cnt_5=0;
 
     for(int i=0;i<10;i++)
     {
+        cin >> A[i];
+        
         if(A[i]%3==0)
             cnt_3++;
         if(A[i]%5==0)
             cnt_5++;
+
     }
+    
     cout << cnt_3 << " " << cnt_5;
     return 0;
 }
