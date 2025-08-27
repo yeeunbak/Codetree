60ac0e412fed17223ad64698c5fdff663753c69a
yeeunbak <67646339+yeeunbak@users.noreply.github.com>
2025-08-27 17:44:52 +0900

    Title: A 부터 B 까지, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250827/A 부터 B 까지/a-to-b.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int A, B;
+    cin >> A >> B;
+
+    while (A<=B)
+    {   
+        cout << A << " ";
+        if(A%2!=0)
+            A*=2;
+        else
+            A+=3;
+    }
+    return 0;
+}
