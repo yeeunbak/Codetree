3b8fea8484ebfafebea62bc5003666789ea17863
yeeunbak <67646339+yeeunbak@users.noreply.github.com>
2025-08-27 17:35:57 +0900

    Title: 0 나올때까지 출력, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250827/0 나올때까지 출력/print-until-zero-is-given.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n=-1;
+    while (n!=0)
+    {   
+        cin >> n;
+        if(n==0)
+            break;
+        cout << n << "\n";
+    }
+    return 0;
+}
