6303942f754e987c5bc73cce7ec3aa7067fbc316
yeeunbak <67646339+yeeunbak@users.noreply.github.com>
2025-08-27 16:45:42 +0900

    Title: 배수의 개수, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

--- 250827/배수의 개수/number-of-multipliers.cpp
+++ 250827/배수의 개수/number-of-multipliers.cpp
@@ -3,14 +3,14 @@ using namespace std;
 
 int main() {
     // Please write your code here.
-    int A[10];
+    int A;
 
     int cnt_3=0;
     int cnt_5=0;
 
     for(int i=0;i<10;i++)
     {
-        cin >> A[i];
+        cin >> A;
         
         if(A[i]%3==0)
             cnt_3++;
@@ -18,7 +18,7 @@ int main() {
             cnt_5++;
 
     }
-    
+
     cout << cnt_3 << " " << cnt_5;
     return 0;
 }
