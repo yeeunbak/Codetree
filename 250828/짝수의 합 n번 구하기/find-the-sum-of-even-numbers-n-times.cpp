3e625b368a330bad65cc909fbc76f686efc6f249
yeeunbak <67646339+yeeunbak@users.noreply.github.com>
2025-08-28 11:29:19 +0900

    Title: 짝수의 합 n번 구하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 250828/짝수의 합 n번 구하기/find-the-sum-of-even-numbers-n-times.cpp
+++ 250828/짝수의 합 n번 구하기/find-the-sum-of-even-numbers-n-times.cpp
@@ -4,14 +4,13 @@ using namespace std;
 int main() {
     // Please write your code here.
     int N;
-    int sum=0;
     cin >> N;
 
     for(int i=0;i<N;i++)
     {   
         int a, b;
         cin >> a >> b;
-        
+
         int sum=0;
         for(a;a<=b;a++)
         {
