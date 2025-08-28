f7484a9beaad3d74786ee8db2f8b0d9398dc608e
yeeunbak <67646339+yeeunbak@users.noreply.github.com>
2025-08-28 11:09:34 +0900

    Title: 정상적인 구구단, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250828/정상적인 구구단/normal-multiple.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int N;
+    cin >> N;
+
+    for(int i=1;i<=N;i++)
+    {
+        for(int j=1;j<=N;j++)
+        {
+            cout << i << " * " << j << " = " << i*j;
+            if(j<N)
+                cout<<", ";
+        }
+        cout<< "\n";
+    }
+    return 0;
+}
