c8079561eddf682b9f73990189a9725640fcb0b6
yeeunbak <67646339+yeeunbak@users.noreply.github.com>
2025-08-28 14:50:21 +0900

    Title: 숫자 피라미드 2, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 250828/숫자 피라미드 2/number-pyramid-2.cpp
+++ 250828/숫자 피라미드 2/number-pyramid-2.cpp
@@ -8,9 +8,9 @@ int main() {
 
     int cnt=0;
 
-    for(int i=1;i<=N;i++)
+    for(int i=1;i<=N;i++)           // N = 1 2 3 4
     {
-        for(int j=0;j<i;j++)
+        for(int j=0;j<i;j++)        
         {
             cnt++;
             cout<< cnt << " ";
@@ -19,3 +19,9 @@ int main() {
     }
     return 0;
 }
+
+//행    숫자
+// N=1  j=0
+// N=2  j=0 1
+// N=3  j=0 1 2
+// N=4  j=0 1 2 3
