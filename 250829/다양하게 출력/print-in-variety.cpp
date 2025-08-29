8ee8e06bc6f531f9ed31a379232b104a43eabe5c
yeeunbak <67646339+yeeunbak@users.noreply.github.com>
2025-08-29 09:49:37 +0900

    Title: 다양하게 출력, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250829/다양하게 출력/print-in-variety.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    cout<<"Total days in Year\n";
+    cout<<"365\n";
+    cout<<"Circumference rate\n";
+    cout<<"3.1415926535";
+    return 0;
+}
