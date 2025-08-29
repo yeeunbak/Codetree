810aa3403256757bbec7449d9a5e17353adee413
yeeunbak <67646339+yeeunbak@users.noreply.github.com>
2025-08-29 09:03:59 +0900

    Title: 좌우로 반복하며 출력, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

--- 250829/좌우로 반복하며 출력/print-in-left-right-reverse.cpp
+++ 250829/좌우로 반복하며 출력/print-in-left-right-reverse.cpp
@@ -12,19 +12,14 @@ int main() {
         {
             //그냥 출력
             for(int j=1;j<=N;j++)
-            {
-            cout << j;
-            }
+                cout << j;
         }
         else
         {
             //반대로 출력
             for(int j=N;j>0;j--)
-            {
                 cout << j;
-            }
         }
-    
         cout<<"\n";
     }
     return 0;
