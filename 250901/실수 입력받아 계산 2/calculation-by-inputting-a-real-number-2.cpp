0f66e428ceb3371111d2b5e17bd969241e5125f7
yeeunbak <67646339+yeeunbak@users.noreply.github.com>
2025-09-01 09:29:22 +0900

    Title: 실수 입력받아 계산 2, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250901/실수 입력받아 계산 2/calculation-by-inputting-a-real-number-2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    double a;
+    cin >> a;
+
+    cout << fixed << setprecision(2);
+    cout << a+1.5;
+    return 0;
+}
