3ab909eae122c8d8d5f5da9f5456651d445a1503
yeeunbak <67646339+yeeunbak@users.noreply.github.com>
2025-09-01 09:06:21 +0900

    Title: 변수 값 교체하기 3, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250901/변수 값 교체하기 3/replacing-variable-values-3.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a = 3, b = 5;
+    int temp;
+
+    temp = a;
+    a = b;
+    b = temp;
+
+    cout << a << "\n" << b;
+    return 0;
+}
