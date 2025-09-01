95000fefe4ac1c69673e633621822a3c3d427209
yeeunbak <67646339+yeeunbak@users.noreply.github.com>
2025-09-01 15:31:00 +0900

    Title: 음수 구별하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250901/음수 구별하기/separate-negative-number.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    cin >> n;
+
+    cout << n;
+    if(n<0)
+    {
+        cout << "\nminus";
+    }
+    return 0;
+}
