11772f73d39b41607bf5a7d2abb6aeabf39ed739
yeeunbak <67646339+yeeunbak@users.noreply.github.com>
2025-09-01 15:37:41 +0900

    Title: 정수의 조건 여부, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 250901/정수의 조건 여부/numbers-condition.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a;
+    cin >> a;
+    if(a>=113)
+        cout << "1"
+    else
+        cout << "0";
+    return 0;
+}
