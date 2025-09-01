9c001542aa0b8197560a536e38954a51d04b6c11
yeeunbak <67646339+yeeunbak@users.noreply.github.com>
2025-09-01 09:51:10 +0900

    Title: 실수 입력받아 계산, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 250901/실수 입력받아 계산/calculation-by-inputting-a-real-number.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    double = a, b;
+    cin >> a >> b;
+
+    cout << fixed << setprecision(2);
+    cout << a+b;
+    return 0;
+}
