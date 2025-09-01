8eaccd1e3ce408438dd90596e4340610327aae69
yeeunbak <67646339+yeeunbak@users.noreply.github.com>
2025-09-01 15:08:22 +0900

    Title: 전화번호 바꾸기, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250901/전화번호 바꾸기/change-phone-number.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n1, n2;
+    char a, b, c, h;
+
+    cin >> a >> b >> c >> h >> n1 >> h >> n2;
+    cout << a << b << c << h << n2 << h << n1;
+    
+    return 0;
+}
