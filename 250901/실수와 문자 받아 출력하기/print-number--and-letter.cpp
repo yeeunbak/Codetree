600702587f3f24bdb1e9fe457b98971c727dabc1
yeeunbak <67646339+yeeunbak@users.noreply.github.com>
2025-09-01 09:57:39 +0900

    Title: 실수와 문자 받아 출력하기, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 250901/실수와 문자 받아 출력하기/print-number--and-letter.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    char c;
+    double a, b;
+
+    cin >> c >> a >> b;
+    cout << c << "\n";
+    cout << fixed << setprecision(2);
+    cout << a << "\n" << b;
+    return 0;
+}
