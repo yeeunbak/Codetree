d1c3b1b8e5f0f4b173880180eed9882fa78de9a6
yeeunbak <67646339+yeeunbak@users.noreply.github.com>
2025-09-01 10:00:39 +0900

    Title: 문자열 순서 바꾸기, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

--- 250901/문자열 순서 바꾸기/change-order-of-strings.cpp
+++ 250901/문자열 순서 바꾸기/change-order-of-strings.cpp
@@ -4,7 +4,13 @@ using namespace std;
 int main() {
     // Please write your code here.
     string S, T;
+    string temp;
     cin >> S >> T;
-    cout << T << "\n" << S;
+
+    temp = S;
+    S = T;
+    T = temp;
+
+    cout << S << "\n" << T;
     return 0;
 }
