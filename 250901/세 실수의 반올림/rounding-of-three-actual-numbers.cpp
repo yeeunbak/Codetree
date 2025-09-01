ced98820090653ecc6b9a3e510c65a35beec4ead
yeeunbak <67646339+yeeunbak@users.noreply.github.com>
2025-09-01 09:53:58 +0900

    Title: 세 실수의 반올림, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250901/세 실수의 반올림/rounding-of-three-actual-numbers.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    double a, b, c;
+    cin >> a >> b >> c;
+
+    cout << fixed << setprecision(3);
+    cout << a << "\n" << b << "\n" << c;
+    return 0;
+}
