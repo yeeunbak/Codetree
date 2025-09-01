5a3f2bb5878525704de5dd9d706a3f93ca3901da
yeeunbak <67646339+yeeunbak@users.noreply.github.com>
2025-09-01 10:23:25 +0900

    Title: 입력받아 출력 2, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250901/입력받아 출력 2/input-and-output-2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n1, n2;
+    char c;
+    cin >> n1 >> c >> n2;
+    cout << n1 << n2;
+    return 0;
+}
