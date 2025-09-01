663300b940111aeb069c9b97ca4004fec32faeb4
yeeunbak <67646339+yeeunbak@users.noreply.github.com>
2025-09-01 15:36:54 +0900

    Title: 시험 통과 여부 확인하기, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250901/시험 통과 여부 확인하기/verify-test-passed.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    cin >> n;
+    if(n>=80)
+        cout << "pass";
+    else
+        cout << 80-n << " more score";
+    return 0;
+}
