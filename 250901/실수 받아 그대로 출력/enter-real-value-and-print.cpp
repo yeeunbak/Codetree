7a253e8cce1672098bdbb6225620f2e443e39758
yeeunbak <67646339+yeeunbak@users.noreply.github.com>
2025-09-01 09:26:17 +0900

    Title: 실수 받아 그대로 출력, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

--- 250901/실수 받아 그대로 출력/enter-real-value-and-print.cpp
+++ 250901/실수 받아 그대로 출력/enter-real-value-and-print.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 int main() {
     // Please write your code here.
     double N;
+    cin >> N;
+
     cout << fixed;
-    cout.setprecision(2);
+    cout << setprecision(2);
 
     cout << N;
     return 0;
