603deef0f34cedfb3709ead3eee4ed454d688e85
yeeunbak <67646339+yeeunbak@users.noreply.github.com>
2025-09-01 09:23:19 +0900

    Title: 실수 받아 그대로 출력, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 250901/실수 받아 그대로 출력/enter-real-value-and-print.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int N;
+    cout << fixed;
+    cout.setprecision(2);
+
+    cout << N;
+    return 0;
+}
