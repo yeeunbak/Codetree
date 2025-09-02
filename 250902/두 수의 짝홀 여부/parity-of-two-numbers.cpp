c4fa1f0b5b6235d2ab91644cd0b9e11e840745d4
yeeunbak <67646339+yeeunbak@users.noreply.github.com>
2025-09-02 10:16:42 +0900

    Title: 두 수의 짝홀 여부, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250902/두 수의 짝홀 여부/parity-of-two-numbers.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a, b;
+    cin >> a >> b;
+    if(a%2==0)
+        cout << "even\n";
+    else
+        cout << "odd\n";
+
+    if(b%2==0)
+        cout << "even";
+    else
+        cout << "odd";
+    return 0;
+}
