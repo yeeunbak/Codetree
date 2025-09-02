8d783421de5ad0ccbf0a072c90307c957fd06940
yeeunbak <67646339+yeeunbak@users.noreply.github.com>
2025-09-02 09:15:40 +0900

    Title: 시력 검사 2, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250902/시력 검사 2/eye-test-2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    double a;
+    cin >> a;
+
+    if(a>=1.0)
+        cout << "High";
+    else if(a>=0.5)
+        cout << "Middle";
+    else
+        cout << "Low";
+    return 0;
+}
