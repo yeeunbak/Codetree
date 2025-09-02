4cb998582d415761aa0ae308546a055c16838396
yeeunbak <67646339+yeeunbak@users.noreply.github.com>
2025-09-02 10:28:16 +0900

    Title: 범위 안의 수, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250902/범위 안의 수/number-in-range.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a;
+    cin >> a;
+    if(a>=10 && a<=20)
+        cout << "yes";
+    else
+        cout << "no";
+    return 0;
+}
