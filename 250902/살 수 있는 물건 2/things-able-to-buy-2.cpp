1e193a376d6e4d0e628a7580cd36e05cc0c3d9d7
yeeunbak <67646339+yeeunbak@users.noreply.github.com>
2025-09-02 09:23:51 +0900

    Title: 살 수 있는 물건 2, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250902/살 수 있는 물건 2/things-able-to-buy-2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    cin >> n;
+
+    if(n>=3000)
+        cout << "book";
+    else if(n<3000 && n>=1000)
+        cout << "mask";
+    else if(n<1000 && n>=500)
+        cout << "pen";
+    else 
+        cout << "no";
+    return 0;
+}
