99a675686bf845986d51dda12f1584920a50120a
yeeunbak <67646339+yeeunbak@users.noreply.github.com>
2025-09-02 10:42:08 +0900

    Title: 세 수의 중간값, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250902/세 수의 중간값/median-of-three-numbers.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    cout << (b>a && b<c) << "\n";
+    return 0;
+}
