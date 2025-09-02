290dba452e5b539e6bdd77844cf33712af1d2571
yeeunbak <67646339+yeeunbak@users.noreply.github.com>
2025-09-02 10:46:03 +0900

    Title: 굉장한 수, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250902/굉장한 수/amazing-number.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    cin >> n;
+    if((n%2!=0 && n%3==0) || (n%2==0 && n%5==0))
+        cout << "true";
+    else
+        cout << "false";
+    return 0;
+}
