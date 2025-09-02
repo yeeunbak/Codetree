79e0aa1b59a8486ab632e860ac2be4d847daa76f
yeeunbak <67646339+yeeunbak@users.noreply.github.com>
2025-09-02 09:20:28 +0900

    Title: 등급 매기기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250902/등급 매기기/ranking.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    cin >> n;
+
+    if(n>=90)
+        cout << "A";
+    else if(n>=80)
+        cout << "B";
+    else if(n>=70)
+        cout << "C";
+    else if(n>=60)
+        cout << "D";
+    else if(n<60)
+        cout << "F";
+    return 0;
+}
