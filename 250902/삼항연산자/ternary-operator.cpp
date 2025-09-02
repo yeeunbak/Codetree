c4d222a36cd0baee38cfbfe96f1e0c17831b67ce
yeeunbak <67646339+yeeunbak@users.noreply.github.com>
2025-09-02 09:12:34 +0900

    Title: 삼항연산자, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250902/삼항연산자/ternary-operator.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    cin >> n;
+    
+    n == 100 ? cout << "pass" : cout << "failure";
+}
