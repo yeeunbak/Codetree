e06ea2b87a3db2babab74327c6bb81e012c9ad59
yeeunbak <67646339+yeeunbak@users.noreply.github.com>
2025-09-02 10:14:07 +0900

    Title: 정수의 조건 여부 2, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250902/정수의 조건 여부 2/numbers-condition-2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a;
+    cin >> a;
+    if(a==5)
+        cout << "A";
+    if(a%2==0)
+        cout << "B";
+    return 0;
+}
