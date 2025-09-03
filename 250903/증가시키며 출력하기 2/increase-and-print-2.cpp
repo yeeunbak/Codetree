95dd4361fd2d735424f7e143c32db01567268588
yeeunbak <67646339+yeeunbak@users.noreply.github.com>
2025-09-03 10:20:26 +0900

    Title: 증가시키며 출력하기 2, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250903/증가시키며 출력하기 2/increase-and-print-2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n=10;
+    while(n<27)
+    {
+        cout << n << " ";
+        n++;
+    }
+    return 0;
+}
