09db8e01b46321d988b1cfc9a1406a1dadbfc842
yeeunbak <67646339+yeeunbak@users.noreply.github.com>
2025-09-03 13:05:47 +0900

    Title: N까지 자연수 출력, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 250903/N까지 자연수 출력/print-1-to-n.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    cin >> n;
+    
+    while(i<n)
+    {
+        cout << i << " ";
+        i++;
+    }
+    return 0;
+}
