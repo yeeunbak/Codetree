af84d9d5e154c848d1fdb85503bb39a8ae00c678
yeeunbak <67646339+yeeunbak@users.noreply.github.com>
2025-09-03 10:16:07 +0900

    Title: 홀수만 출력, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250903/홀수만 출력/output-only-odd.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a, b;
+    cin >> a >> b;
+    for(a;a<=b;a+=2)
+        cout << a << " ";
+    return 0;
+}
