2ff80a81f3aec200224fe2161c331477d69ac58f
yeeunbak <67646339+yeeunbak@users.noreply.github.com>
2025-09-03 13:16:03 +0900

    Title: n부터 1까지 정수 출력, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250903/n부터 1까지 정수 출력/print-n-to-1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    cin >> n;
+    while(n>=1)
+    {
+        cout << n << " ";
+        n--;
+    }
+
+    return 0;
+}
