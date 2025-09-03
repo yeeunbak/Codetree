5342440127b58760ab676b5c2cdd2a54bbe465db
yeeunbak <67646339+yeeunbak@users.noreply.github.com>
2025-09-03 13:13:57 +0900

    Title: 짝수만 출력, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 250903/짝수만 출력/output-only-even.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a, b;
+    cin >> a >> b;
+    whie(a<=b)
+    {
+        cout << a << " ";
+        a+=2;
+    }
+    return 0;
+}
