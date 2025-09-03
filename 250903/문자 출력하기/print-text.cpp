5857d336676c40415e6264e71c95aa544d8174ed
yeeunbak <67646339+yeeunbak@users.noreply.github.com>
2025-09-03 13:18:13 +0900

    Title: 문자 출력하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250903/문자 출력하기/print-text.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    char c;
+    cin >> c;
+    for(int i=0;i<8;i++)
+    {
+        cout << c;
+    }
+    return 0;
+}
