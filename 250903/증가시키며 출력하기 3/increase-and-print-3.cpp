19ef17cb3b82c26c1ed809678354196ebd400a0c
yeeunbak <67646339+yeeunbak@users.noreply.github.com>
2025-09-03 13:10:06 +0900

    Title: 증가시키며 출력하기 3, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 250903/증가시키며 출력하기 3/increase-and-print-3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n=10;
+    while(n<=26)
+    {
+        cout << n;
+        i+=2;
+    }
+    return 0;
+
+}
