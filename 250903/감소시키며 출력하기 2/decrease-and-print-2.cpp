3b5c7908c4934635c0ecd967f573a96bbed2655b
yeeunbak <67646339+yeeunbak@users.noreply.github.com>
2025-09-03 13:15:16 +0900

    Title: 감소시키며 출력하기 2, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250903/감소시키며 출력하기 2/decrease-and-print-2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n=26;
+    while(n>=10)
+    {
+        cout << n << " ";
+        n--;
+    }
+    return 0;
+}
