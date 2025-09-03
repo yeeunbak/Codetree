e167d6e39ee790d85447f31b122ad6b2973084b0
yeeunbak <67646339+yeeunbak@users.noreply.github.com>
2025-09-03 13:19:55 +0900

    Title: A/B 출력, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250903/A/B 출력/a-divide-b.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a, b;
+    cin >> a >> b;
+    double c = a/b;
+    cout << fixed << setprecision(21);
+    cout << c;
+    return 0;
+}
