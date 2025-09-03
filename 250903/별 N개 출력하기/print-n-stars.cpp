3d1ae0617444750ad1bf74cb653691856c9be73d
yeeunbak <67646339+yeeunbak@users.noreply.github.com>
2025-09-03 13:08:02 +0900

    Title: 별 N개 출력하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250903/별 N개 출력하기/print-n-stars.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    cin >> n;
+
+    int i=0;
+    while(i<n)
+    {
+        cout << "*\n";
+        i++;
+    }
+    return 0;
+}
