3ea3374755f9f416d09f9fb6ba3d265c9f62c4e9
yeeunbak <67646339+yeeunbak@users.noreply.github.com>
2025-09-03 10:13:45 +0900

    Title: 1부터 n까지 출력, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250903/1부터 n까지 출력/print-from-1-to-n.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    cin >> n;
+    for(int i=1;i<=n;i++)
+        cout << i << " ";
+    return 0;
+}
