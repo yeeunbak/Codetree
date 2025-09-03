759509e54f89ce8ccab1b9910b5d2e6186810245
yeeunbak <67646339+yeeunbak@users.noreply.github.com>
2025-09-03 10:18:18 +0900

    Title: B부터 A까지 감소, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250903/B부터 A까지 감소/dec-from-b-to-a.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a, b;
+    cin >> a >> b;
+
+    for(b;b>=a;b--)
+        cout << b << " ";
+    return 0;
+}
