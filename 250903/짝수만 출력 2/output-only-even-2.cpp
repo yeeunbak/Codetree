dab093b31111a6d0f204abcdc1db2f0a5e436d50
yeeunbak <67646339+yeeunbak@users.noreply.github.com>
2025-09-03 13:16:39 +0900

    Title: 짝수만 출력 2, Time: 1080ms, Memory: 80MB, Status: Time Limit Exceed - Codetree

---
+++ 250903/짝수만 출력 2/output-only-even-2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int b, a;
+    while(b<=a)
+    {
+        cout << b << " ";
+        b-=2;
+    }
+    return 0;
+}
