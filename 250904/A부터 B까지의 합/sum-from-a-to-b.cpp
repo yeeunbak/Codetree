aaee6f80962da6c09756377ec0f1c0915c05db3d
yeeunbak <67646339+yeeunbak@users.noreply.github.com>
2025-09-04 16:49:12 +0900

    Title: A부터 B까지의 합, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250904/A부터 B까지의 합/sum-from-a-to-b.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a, b;
+    cin >> a >> b;
+
+    int sum=0;
+    for(a;a<=b;a++)
+    {
+        sum=+a;
+    }
+    cout << sum;
+    return 0;
+}
