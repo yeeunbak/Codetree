6cd17889c518f87d4b45b593650b53d3c463645b
yeeunbak <67646339+yeeunbak@users.noreply.github.com>
2025-09-04 15:19:47 +0900

    Title: 높은 수에서 낮은 수까지, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250904/높은 수에서 낮은 수까지/from-high-to-low.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a, b;
+    cin >> a >> b;
+
+    if(a>b)
+    {
+        for(int i=a;i>=b;i--)
+        {
+            cout << i << " ";
+        }
+    }
+    else
+    {
+        for(int i=b;i>=a;i--)
+        {
+            cout << i << " ";
+        }
+    }
+    return 0;
+}
