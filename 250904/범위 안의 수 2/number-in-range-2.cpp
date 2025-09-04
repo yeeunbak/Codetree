8cf4b61955a132f4c35695ca03254b6b0b194268
yeeunbak <67646339+yeeunbak@users.noreply.github.com>
2025-09-04 17:10:32 +0900

    Title: 범위 안의 수 2, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250904/범위 안의 수 2/number-in-range-2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    int sum=0;
+    int cnt=0;
+    for(int i=0;i<10;i++)
+    {
+        cin >> n;
+        sum+=n;
+        cnt++;
+    }
+    double avg = double(sum)/double(cnt);
+    cout << sum << " ";
+    cout << fixed << setprecision(1) << avg;
+    return 0;
+}
