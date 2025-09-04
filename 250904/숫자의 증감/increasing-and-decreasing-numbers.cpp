1af6213e7ba829939d80b8f952a59be419431aa5
yeeunbak <67646339+yeeunbak@users.noreply.github.com>
2025-09-04 13:51:18 +0900

    Title: 숫자의 증감, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 250904/숫자의 증감/increasing-and-decreasing-numbers.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    char c;
+    int n;
+    cin >> c >> n;
+
+    if(c=='A')
+    {
+        for(int i=1;i<=N;i++)
+            cout << i << " ";
+    }
+    if(c=='D')
+    {
+        for(int i=N;i>=N;i--)
+            cout << i << " ";
+    }
+    return 0;
+}
