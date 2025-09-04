086b515353fef2e1fdfdbdac067808d2e07a64ea
yeeunbak <67646339+yeeunbak@users.noreply.github.com>
2025-09-04 16:41:57 +0900

    Title: 청소당번 정하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 250904/청소당번 정하기/cleaning-numbering.cpp
+++ 250904/청소당번 정하기/cleaning-numbering.cpp
@@ -7,13 +7,13 @@ int main() {
     cin >> day;
     int c_room=0, hall=0, toi=0;
 
-    for(int i=0;i<=day;i++)
+    for(int i=1;i<=day;i++)
     {
         if(i%2==0 && i%3!=0 && i%12!=0)
             c_room++;
         else if(i%3==0 && i%12!=0)
             hall++;
-        else if(i%12==0 && i!=0) // 0%12==0 이 경우 제외 
+        else if(i%12==0)
             toi++;
     }
     cout << c_room << " " << hall << " " << toi;
