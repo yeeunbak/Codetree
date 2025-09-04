e0833e345b075624e843deaf08c9220f05657873
yeeunbak <67646339+yeeunbak@users.noreply.github.com>
2025-09-04 15:20:30 +0900

    Title: 자연수면 출력, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250904/자연수면 출력/output-only-natural-number.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a, b;
+    cin >> a >> b;
+
+    if(a>0)
+    {
+        for(int i=0;i<b;i++)
+            cout << a;
+    }
+    return 0;
+}
