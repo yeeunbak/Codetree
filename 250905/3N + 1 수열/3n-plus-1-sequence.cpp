e7622e1229fbc9e83e0806a70a2f2d6adb2a87a0
yeeunbak <67646339+yeeunbak@users.noreply.github.com>
2025-09-05 10:56:59 +0900

    Title: 3N + 1 수열, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250905/3N + 1 수열/3n-plus-1-sequence.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    cin >> n;
+    int cnt=0;
+    while(1)
+    {
+        if(n!=1)
+        {
+            if(n%2==0)
+            {
+                n = n/2;
+                cnt++;
+            }
+            else
+            {
+                n = (n*3)+1;
+                cnt++;
+            }
+        }
+        else
+            break;
+    }
+    cout << cnt;
+    return 0;
+}
