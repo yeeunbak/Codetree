6016eb89636262d101bb2410788d9dd25baac952
yeeunbak <67646339+yeeunbak@users.noreply.github.com>
2025-09-05 09:45:51 +0900

    Title: 1까지 나누기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250905/1까지 나누기/divide-by-1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    cin >> n;
+    int cnt=0;
+    for(int i=1;i<=5000;i++)
+    {
+        n /= i;
+        cnt++;
+        if(n<=1)
+        {
+            cout << cnt;
+            break;
+        }
+    }
+    return 0;
+}
