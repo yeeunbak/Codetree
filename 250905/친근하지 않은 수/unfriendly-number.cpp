9c60433be91459d84fb12fd50590a59d3b359741
yeeunbak <67646339+yeeunbak@users.noreply.github.com>
2025-09-05 09:10:39 +0900

    Title: 친근하지 않은 수, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250905/친근하지 않은 수/unfriendly-number.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    cin >> n;
+    int cnt=0;
+    for(int i=1;i<=n;i++)
+    {
+        if(i%2==0 || i%3==0 || i%5==0)
+            continue;
+        else
+            cnt++;
+    }
+    cout << cnt;
+    return 0;
+}
