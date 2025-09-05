dd7bc1f0bb13d7684f0e1237d677f1d6b2193e18
yeeunbak <67646339+yeeunbak@users.noreply.github.com>
2025-09-05 09:23:20 +0900

    Title: 특정 조건의 온전수 구하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250905/특정 조건의 온전수 구하기/print-onjeonsu.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    cin >> n;
+
+    for(int i=1;i<=n;i++)
+    {
+        if(i%2==0 || i%10==5 || (i%3==0 && i%9!=0))
+            continue;
+        else
+            cout << i << " ";
+    }
+    return 0;
+}
