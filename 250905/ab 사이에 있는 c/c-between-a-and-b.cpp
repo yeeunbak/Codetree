5ccac8083a9b577612250fc7140538b4dc172a8a
yeeunbak <67646339+yeeunbak@users.noreply.github.com>
2025-09-05 14:03:19 +0900

    Title: ab 사이에 있는 c, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250905/ab 사이에 있는 c/c-between-a-and-b.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a, b, c;
+    cin >> a >> b >> c;
+    bool satisfied = false;
+
+    for(int i=a;i<=b;i++)
+    {
+        if(i%c==0)
+        {
+            satisfied = true;
+        }
+    }
+
+    if(satisfied == true)
+        cout << "YES";
+    else
+        cout << "NO";
+    return 0;
+}
