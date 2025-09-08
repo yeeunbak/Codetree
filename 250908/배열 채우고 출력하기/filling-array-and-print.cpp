9f26267b421299045e3fb755017c81651714cb7a
yeeunbak <67646339+yeeunbak@users.noreply.github.com>
2025-09-08 14:03:52 +0900

    Title: 배열 채우고 출력하기, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 250908/배열 채우고 출력하기/filling-array-and-print.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    char arr[10];
+
+    for(int i=0;i<10;i++)
+        cin >> arr[i];
+    for(int i=0li<10;i++)
+        cout << arr[9-i];
+    return 0;
+}
