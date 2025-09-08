ade65eb088779bb769b46b8f137dfed8af7c9fac
yeeunbak <67646339+yeeunbak@users.noreply.github.com>
2025-09-08 14:12:27 +0900

    Title: 특정 수 도달하기, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

--- 250908/특정 수 도달하기/reaching-specific-number.cpp
+++ 250908/특정 수 도달하기/reaching-specific-number.cpp
@@ -11,14 +11,10 @@ int main() {
     for(int i=0;i<10;i++)
     {
         cin >> arr[i];
-    }
-
-    for(int i=0;i<10;i++)
-    {
         if(arr[i]>=250)
             break;
         sum+=arr[i];
-        cnt++;
+        cnt++;        
     }
 
     double avg = double(sum)/cnt;
