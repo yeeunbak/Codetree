b7e2827f771d141afce3ccfa0cb57e8bc479eaa8
yeeunbak <67646339+yeeunbak@users.noreply.github.com>
2025-09-10 17:45:39 +0900

    Title: 구구단 만들기 2, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250910/구구단 만들기 2/print-multiplication-table-2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a, b;
+    cin >> a >> b;
+
+    for(int i=2;i<=8;i+=2)
+    {
+        for(int j=b;j>=a;j--)
+        {
+            cout << j << " * " << i << " = " << j*i;
+            if(j>a)
+                cout << " / ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
