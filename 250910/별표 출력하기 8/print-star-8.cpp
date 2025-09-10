c0c15031b13cd31dacab02a16f4806b57b6e0a9d
yeeunbak <67646339+yeeunbak@users.noreply.github.com>
2025-09-10 10:04:25 +0900

    Title: 별표 출력하기 8, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250910/별표 출력하기 8/print-star-8.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    cin >> n;
+    for(int i=0;i<n;i++)
+    {
+        if(i%2!=0)
+        {
+            for(int j=0;j<i+1;j++) cout << "* ";
+            cout << "\n";
+        }
+        else
+            cout << "*\n";
+
+    }
+    return 0;
+}
