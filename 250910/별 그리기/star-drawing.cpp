586a2226b2705ee2484bc9a0c5aa46d2d84cbc2a
yeeunbak <67646339+yeeunbak@users.noreply.github.com>
2025-09-10 09:33:03 +0900

    Title: 별 그리기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250910/별 그리기/star-drawing.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    
+    int n;
+    cin >> n;
+    int cnt=n;
+
+    for(int i=0;i<2*n-1;i++)
+    {
+        if(i<n)
+        {
+            for(int j=0;j<cnt-1;j++)
+                cout << " ";            
+            for(int j=0;j<2*i+1;j++)
+                cout << "*";
+            cnt--;
+        }
+        else
+        {
+            for(int j=0;j<cnt+1;j++)
+                cout << " ";
+            for(int j=0;j<(2*n-1)-2*(i-n)-2;j++)
+                cout << "*";
+            cnt++;
+        }
+        cout << "\n";
+    }    
+    return 0;
+}
