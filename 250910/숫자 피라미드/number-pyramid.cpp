9ef430eed57dc4819f03ad1090d41efbffa76381
yeeunbak <67646339+yeeunbak@users.noreply.github.com>
2025-09-10 17:51:05 +0900

    Title: 숫자 피라미드, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250910/숫자 피라미드/number-pyramid.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    cin >> n;
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=i;j++)
+            cout << i << " ";
+        cout << "\n";
+    }
+    return 0;
+}
