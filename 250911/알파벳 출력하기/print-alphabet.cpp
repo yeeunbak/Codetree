4f7fa248f818c029373f9e0f08c6f2bb374c6229
yeeunbak <67646339+yeeunbak@users.noreply.github.com>
2025-09-11 09:46:36 +0900

    Title: 알파벳 출력하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250911/알파벳 출력하기/print-alphabet.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    cin >> n;
+
+    char x = 'A';
+
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<=i;j++)
+        {
+            cout << char(x);
+            if(x=='Z')
+                x = 'A';
+            else
+                x++;
+        }
+        cout << "\n";
+    }
+    return 0;
+}
