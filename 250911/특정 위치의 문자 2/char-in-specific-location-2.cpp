8cb17e9e41f75c871da6c783da3fe3700d1ebba9
yeeunbak <67646339+yeeunbak@users.noreply.github.com>
2025-09-11 16:11:48 +0900

    Title: 특정 위치의 문자 2, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250911/특정 위치의 문자 2/char-in-specific-location-2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    char arr[10];
+    for(int i=0;i<10;i++)
+        cin >> arr[i];
+
+    cout << arr[1] << " " << arr[4] << " " << arr[7];
+    return 0;
+}
