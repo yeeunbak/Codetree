faba0989c689606594aac56b01674bc45079dd6d
yeeunbak <67646339+yeeunbak@users.noreply.github.com>
2025-09-11 10:52:16 +0900

    Title: 홀수로 이루어진 정사각형, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250911/홀수로 이루어진 정사각형/square-with-odd-numbers.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    cin >> n;
+
+    for(int i=0;i<n;i++)
+    {
+        int val = 11+2*i;
+        for(int j=0;j<n;j++)
+        {
+            cout << val << " ";
+            val+=2;
+        }
+        cout << "\n";
+    }
+    return 0;
+}
