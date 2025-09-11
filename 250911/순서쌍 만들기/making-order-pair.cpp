53c83a95a0f59c6f74f714d85ad2b0e3f32b2311
yeeunbak <67646339+yeeunbak@users.noreply.github.com>
2025-09-11 10:42:52 +0900

    Title: 순서쌍 만들기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250911/순서쌍 만들기/making-order-pair.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    cin >> n;
+
+    for(int i=n;i>0;i--)
+    {
+        for(int j=n;j>0;j--)
+            cout << "(" << i << ","<< j << ") ";
+        cout << "\n";
+    }
+    return 0;
+}
