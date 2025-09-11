b17b8f4e81f16df0af07f5cfbd0e630943f71fac
yeeunbak <67646339+yeeunbak@users.noreply.github.com>
2025-09-11 15:56:18 +0900

    Title: 특정 위치의 수, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250911/특정 위치의 수/numbers-in-specific-location.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int arr[10];
+    for(int i=0;i<10;i++)
+        cin >> arr[i];
+
+    cout << arr[2]+arr[4]+arr[9];
+    return 0;
+}
