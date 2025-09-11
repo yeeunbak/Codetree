9a7ee19c036466391fc79502c872d57ad3e2001c
yeeunbak <67646339+yeeunbak@users.noreply.github.com>
2025-09-11 16:53:40 +0900

    Title: 배열에서 원소의 특징, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 250911/배열에서 원소의 특징/characteristics-of-elements-in-an-array.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int arr[10];
+
+    for(int i=0;i<10;i++)
+    {
+        if(arr[i]%3==0)
+        {
+            cout << arr[i-1];
+            break;
+        \}
+    \}
+    return 0;
+\}
+
+$0
+        cin >> arr[i];
+        if(arr[i]%3==0)
+        {
+            cout << arr[i-1];
+            break;
+        }
+    }
+    return 0;
+}
