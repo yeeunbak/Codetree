4e4f69f1209ad34da22045f401bbfa88c1de4f06
yeeunbak <67646339+yeeunbak@users.noreply.github.com>
2025-09-11 14:52:09 +0900

    Title: 좌우 지그재그 출력, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250911/좌우 지그재그 출력/print-lr-zigzag.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    cin >> n;
+    
+    int cnt = 1;
+
+    for(int i=0;i<n;i++)
+    {
+        if(i%2==0)
+        {
+            for(int j=0;j<n;j++)
+                cout << i*n + (j+1) << " ";
+        }
+        else
+        {
+            for(int j=n;j>0;j--)
+                cout << i*n + j << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
