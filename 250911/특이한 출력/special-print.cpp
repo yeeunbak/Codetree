3bddfbb508da707456fc37965bfd7299921d25a4
yeeunbak <67646339+yeeunbak@users.noreply.github.com>
2025-09-11 09:15:27 +0900

    Title: 특이한 출력, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250911/특이한 출력/special-print.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    cin >> n;
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=n;j++)
+        {
+            if((i+j)%4==0)
+                cout << "(" << i << ", " << j << ") " << "\n";
+            else
+                cout << "(" << i << ", " << j << ") ";
+        }
+    }
+    return 0;
+}
