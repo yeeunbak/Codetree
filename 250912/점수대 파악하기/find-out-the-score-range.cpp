a4c033c8dffcc92f05876ec21f9eea08cc97b9d1
yeeunbak <67646339+yeeunbak@users.noreply.github.com>
2025-09-12 10:23:33 +0900

    Title: 점수대 파악하기, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250912/점수대 파악하기/find-out-the-score-range.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int arr[100];
+    int arr_cnt[10]={}; //i= 0 1 2 3 4 5 6 7 8 9
+
+    int cnt = 0;
+
+    for(int i=0;i<100;i++)
+    {
+        cin >> arr[i];
+        if(arr[i]==0)
+            break;
+        cnt++;
+    }
+
+    for(int i=0;i<cnt;i++)
+    {
+        if(arr[i]>10)
+            arr_cnt[(arr[i]/10)-1]++;
+    }
+    
+    for(int i=0;i<10;i++)
+        cout << 100-10*i << " - " << arr_cnt[9-i] << "\n";
+    
+    return 0;
+}
