706170a03fd0c543b9861c8d944f27719e0a2d3f
yeeunbak <67646339+yeeunbak@users.noreply.github.com>
2025-09-12 09:51:11 +0900

    Title: 1-9 개수 세기, Time: 16ms, Memory: 0MB, Status: Runtime Error - Codetree

---
+++ 250912/1-9 개수 세기/count-one-to-nine.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    cin >> n;
+    int arr[10];
+    int arr_cnt[10] = {0, };
+
+    for(int i=0;i<n;i++)
+        cin >> arr[i];
+    
+    for(int i=0;i<n;i++)
+        arr_cnt[arr[i]]++;
+
+    for(int i=1;i<10;i++)
+        cout << arr_cnt[i] << "\n";
+
+    return 0;
+}
