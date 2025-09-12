8716e9e344b9f836ff3ef68ba16b30c64175e8b0
yeeunbak <67646339+yeeunbak@users.noreply.github.com>
2025-09-12 10:03:26 +0900

    Title: 주사위 놀이, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 250912/주사위 놀이/play-with-dice.cpp
+++ 250912/주사위 놀이/play-with-dice.cpp
@@ -4,11 +4,11 @@ using namespace std;
 int main() {
     // Please write your code here.
     int arr[10];
-    int arr_cnt[6]={0, 0, 0, 0, 0, 0};
+    int arr_cnt[6]={0, 0, 0, 0, 0, 0}; // i = 0 1 2 3 4 5
     for(int i=0;i<10;i++)
     {    
         cin >> arr[i];
-        arr_cnt[arr[i]-1]++;
+        arr_cnt[arr[i]-1]++;            // i = 1 2 3 4 5 6 -> 0 1 2 3 4 5
     }
 
     for(int i=0;i<6;i++)
