eab31672384fb03f01e9f287db50e3f918b72b11
yeeunbak <67646339+yeeunbak@users.noreply.github.com>
2025-09-15 09:39:05 +0900

    Title: 3N + 1 수열 M번, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250915/3N + 1 수열 M번/3n-plus-1-sequence-m-times.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    cin >> n;
+    
+    int val;
+
+    for(int i=0;i<n;i++)
+    {
+        cin >> val;
+        int cnt = 0;
+        while(val!=1)
+        {
+            if(val%2==0)
+                val=val/2;
+            else
+                val=(val*3)+1;
+            cnt++;
+        }
+        cout << cnt << "\n";
+    }
+    return 0;
+}
