d905aaac1a8b9de3a9829506c5d38a13922b477d
yeeunbak <67646339+yeeunbak@users.noreply.github.com>
2025-09-17 14:34:04 +0900

    Title: 문자열의 길이 출력하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250917/문자열의 길이 출력하기/print-strings-length.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    string str1;
+    string str2;
+    cin >> str1 >> str2;
+    cout << str1.length()+str2.length();
+    return 0;
+}
