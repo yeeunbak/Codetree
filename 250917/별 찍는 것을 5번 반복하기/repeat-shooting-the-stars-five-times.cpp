ed3fd71262d76afac7284eca13a6b18296f2291f
yeeunbak <67646339+yeeunbak@users.noreply.github.com>
2025-09-17 16:57:33 +0900

    Title: 별 찍는 것을 5번 반복하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250917/별 찍는 것을 5번 반복하기/repeat-shooting-the-stars-five-times.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+void Print10Stars()
+{
+    for(int i=0;i<10;i++)
+        cout << "*";
+    cout << "\n";
+}
+
+int main() {
+    // Please write your code here.
+    for(int i=0;i<5;i++)
+        Print10Stars();
+    
+    return 0;
+}
