354dbebf6ea47f99fec4dc3705213447692aa592
yeeunbak <67646339+yeeunbak@users.noreply.github.com>
2025-09-17 14:17:44 +0900

    Title: 동전이 있는 위치, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250917/동전이 있는 위치/Where-coins-are-located.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n, m;
+    cin >> n >> m;
+
+    int arr[10][10]={0, };
+    for(int i=0;i<m;i++)
+    {
+        int r,c;
+        cin >> r >> c;
+
+        arr[r-1][c-1] = 1;
+    }
+
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n;j++)
+            cout << arr[i][j] << " ";
+        cout << "\n";
+    }
+
+    return 0;
+}
