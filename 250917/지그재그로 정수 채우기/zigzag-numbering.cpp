9c5b839b2a9eb85f61fae8bc1a14fa68a312b238
yeeunbak <67646339+yeeunbak@users.noreply.github.com>
2025-09-17 11:06:51 +0900

    Update zigzag-numbering.cpp

--- 250917/지그재그로 정수 채우기/zigzag-numbering.cpp
+++ 250917/지그재그로 정수 채우기/zigzag-numbering.cpp
@@ -1,3 +1,4 @@
+//배열X
 #include <iostream>
 
 using namespace std;
@@ -16,6 +17,41 @@ int main() {
             else
                 cout << (n-1-i)+(j*n) << " ";
         }
+        cout << "\n";
+    }
+    return 0;
+}
+
+//배열로 저장
+#include <iostream>
+
+using namespace std;
+
+int n, m;
+
+int main() {
+    cin >> n >> m;
+    // Please write your code here.
+    int arr[100][100];
+    int num = 0;
+    for(int j=0;j<m;j++)
+    {
+        if(j%2==0)
+        {
+            for(int i=0;i<n;i++)
+                arr[i][j] = num++;
+        }
+        else
+        {
+            for(int i=n-1;i>=0;i--)
+                arr[i][j] = num++;
+        }
+    }
+
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<m;j++)
+            cout << arr[i][j] << " ";
         cout << "\n";
     }
     return 0;
