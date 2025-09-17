5a9ea1a5571d84e89a91d93064cd4dfaec6156bb
yeeunbak <67646339+yeeunbak@users.noreply.github.com>
2025-09-17 09:48:16 +0900

    Title: 지그재그로 정수 채우기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250917/지그재그로 정수 채우기/zigzag-numbering.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int n, m;
+
+int main() {
+    cin >> n >> m;
+    // Please write your code here.
+    int arr[100][100];
+    for(int i=0;i< n;i++)
+    {
+        for(int j=0;j<m;j++)
+        {
+            if(j%2==0)
+                cout << i+(j*n) << " ";
+            else
+                cout << (n-1-i)+(j*n) << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+ 
