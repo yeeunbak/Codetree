fb1583554878ace6226b027c97bd4faf49f26049
yeeunbak <67646339+yeeunbak@users.noreply.github.com>
2025-09-17 14:24:59 +0900

    Title: 격자 모양 출력하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 250917/격자 모양 출력하기/print-grid-shape.cpp
+++ 250917/격자 모양 출력하기/print-grid-shape.cpp
@@ -6,19 +6,21 @@ int main() {
     int n, m;
     cin >> n >> m;
 
-    int arr[10][10];
+    int arr[10][10]={0, };
 
     for(int i=0;i<m;i++)
     {
         int r, c;
         cin >> r >> c;
 
-        arr[r-1][c-1] = (r-1)*(c-1);
+        arr[r-1][c-1] = r*c;
     } 
 
     for(int i=0;i<n;i++)
+    {
         for(int j=0;j<n;j++)
             cout << arr[i][j] << " ";
         cout << "\n";
+    }
     return 0;
 }
