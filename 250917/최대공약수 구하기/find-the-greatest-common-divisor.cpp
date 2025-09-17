0fd438a55cf9d992f1c233ea53702605a4b5d770
yeeunbak <67646339+yeeunbak@users.noreply.github.com>
2025-09-17 17:36:17 +0900

    Update find-the-greatest-common-divisor.cpp

--- 250917/최대공약수 구하기/find-the-greatest-common-divisor.cpp
+++ 250917/최대공약수 구하기/find-the-greatest-common-divisor.cpp
@@ -24,3 +24,30 @@ int main() {
 
     return 0;
 }
+
+
+//comparison
+#include <iostream>
+
+using namespace std;
+
+int n, m;
+
+void maxnum(int n, int m){
+
+    int num;
+    for(int i=1;i<1000;i++)
+    {
+        if((n%i==0) && (m%i==0))
+            num = i;
+    }
+    cout << num;
+}
+
+int main() {
+    cin >> n >> m;
+    maxnum(n, m);
+    // Please write your code here.
+
+    return 0;
+}
