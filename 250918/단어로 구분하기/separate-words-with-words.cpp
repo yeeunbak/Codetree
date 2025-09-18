801eb854c8906c187ab98e26148c66d1d51d9086
yeeunbak <67646339+yeeunbak@users.noreply.github.com>
2025-09-18 10:03:39 +0900

    Title: 단어로 구분하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250918/단어로 구분하기/separate-words-with-words.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    string arr[10];
+    for(int i=0;i<10;i++)
+    {
+        cin >> arr[i];
+        cout << arr[i] << "\n";
+    }
+    return 0;
+}
