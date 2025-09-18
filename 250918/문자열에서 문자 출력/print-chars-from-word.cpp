c637c74c296206b210e31c22c20c87838bc78dd2
yeeunbak <67646339+yeeunbak@users.noreply.github.com>
2025-09-18 10:49:08 +0900

    Title: 문자열에서 문자 출력, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250918/문자열에서 문자 출력/print-chars-from-word.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    string arr;
+    cin >> arr;
+
+    for(int i=0;i<arr.length();i++)
+        cout << arr[i] << "\n";
+    return 0;
+}
