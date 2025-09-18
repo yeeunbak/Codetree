7131c4e965118f53c4c14ddc505283c36468ea2c
yeeunbak <67646339+yeeunbak@users.noreply.github.com>
2025-09-18 10:07:10 +0900

    Title: 단어로 구분하기 3, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 250918/단어로 구분하기 3/separate-words-with-words-3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    string str[10];
+    for(int i=0;i<10;i++)
+    {
+        cin >> arr[i];
+        cout << arr[n-1-i] << "\n";
+    }
+    return 0;
+}
