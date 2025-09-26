9793dbae4cfa2993736cc7446cd3d34cc93f0927
yeeunbak <67646339+yeeunbak@users.noreply.github.com>
2025-09-26 11:40:02 +0900

    Title: 아스키코드표 맞추기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250926/아스키코드표 맞추기/chart-of-ASCII.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    for(int i=0;i<5;i++)
+    {
+        int n;
+        cin >> n;
+        cout << (char)n << " ";
+    }
+    return 0;
+}
