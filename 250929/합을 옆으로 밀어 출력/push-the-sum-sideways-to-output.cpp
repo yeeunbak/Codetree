56c994b908b9bb4e9513b2be3a00043c050cfe28
yeeunbak <67646339+yeeunbak@users.noreply.github.com>
2025-09-29 10:38:32 +0900

    Title: 합을 옆으로 밀어 출력, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250929/합을 옆으로 밀어 출력/push-the-sum-sideways-to-output.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    cin >> n;
+
+    int sum = 0;
+
+    for(int i=0;i<n;i++)
+    {
+        int num;
+        cin >> num;
+        sum += num;
+    }
+
+    string str = to_string(sum);
+
+    string ans = str.substr(1, str.size()-1) + str.substr(0, 1);
+
+    cout << ans;
+    return 0;
+}
