fcf518a54fe0e3656a78e1118d8de594b72cbdd0
yeeunbak <67646339+yeeunbak@users.noreply.github.com>
2025-09-29 09:26:05 +0900

    Title: 소문자와 숫자, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250929/소문자와 숫자/letter-and-number.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    string str;
+    cin >> str;
+
+    for(int i=0;i<str.size();i++)
+    {
+        if(isalpha(str[i]))
+        {
+            if(str[i]>='A' && str[i]<='Z')
+                cout << (char)(str[i]-'A'+'a');
+            else
+                cout << str[i];
+        }
+        else if(isdigit(str[i]))
+        {
+            cout << str[i];
+        }
+    }
+    return 0;
+}
