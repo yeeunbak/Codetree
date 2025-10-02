c50793835727477b846b3a11c9f0d4c5f946d85e
yeeunbak <67646339+yeeunbak@users.noreply.github.com>
2025-10-02 09:43:59 +0900

    Title: 미는 횟수, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 251002/미는 횟수/number-of-pushes.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    string A, B;
+    cin >> A >> B;
+
+    int L = A.length();
+    int cnt = 0;
+    bool is_same = false;
+
+    for(int i=0;i<L;i++)
+    {
+        B = B.substr(L-1, 1) + B.substr(0, L-1);
+        cnt++;
+        if(A==B)
+        {
+            is_same = true;
+            cout << cnt;
+            break;
+        }
+    }
+
+    if(is_same==false)
+        cout << "-1";
+        
+    return 0;
+}
